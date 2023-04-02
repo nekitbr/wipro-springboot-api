e687d36f1f54823e28bd48ee58e670a85e6cc419
José Dall'Asta <nekitbr3131@gmail.com>
2023-04-02 10:19:28 -0300

    implement 404 exception

---
+++ src/main/java/com/jose/api/exceptions/NotFoundException.java
@@ -0,0 +1,13 @@
+package com.jose.api.exceptions;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends ResponseStatusException {
+    public NotFoundException(String message) {
+        super(HttpStatus.NOT_FOUND, message);
+    }
+}
